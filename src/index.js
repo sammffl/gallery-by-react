239db2470e27bf5d08d693e636f5c12d11813f1d
shenyu <samfec@163.com>
2016-09-23 18:56:03 +0800

    add redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  createStore,
+  combineReducers,
+  applyMiddleware
+} from 'redux';
+
+
 import App from './components/Main';
 
 // Render the main component into the dom
