8639ea68ac089b4876dc1a9bf72df207405bc5b5
shenyu <samfec@163.com>
2016-09-23 22:42:11 +0800

    添加reducer和action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,10 @@
  * Created by SamMFFL on 2016/9/23.
  */
 import {combineReducers} from 'redux';
+import todos from './todos';
 
-const reducers = combineReducers({});
+const reducers = combineReducers({
+    todos,
+});
 
 export default reducers;
